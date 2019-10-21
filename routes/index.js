e7444b76c9c71f05b8be68461ffe04bc649bb616
Nishay Madhani <nshmadhani@gmail.com>
2019-10-21 20:56:04 +0530

    Dept Authentication

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
     app.use('/auth',require('./auth'));
     app.use('/user',passport.authenticate('jwt'),require('./user'));
     app.use('/committee',passport.authenticate('jwt'),require('./committee'));
-    app.use('/department',passport.authenticate('jwt'),require('./department'));
+    app.use('/department',require('./department'));
     app.use('/events',passport.authenticate('jwt'),require('./events'));
     app.use('/rooms',require('./rooms'));
     
