c47d11ef1cf6c333f57942b4851ca75adf6b7215
Nishay Madhani <nshmadhani@gmail.com>
2019-10-21 22:58:48 +0530

    Public and stuff

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-
+const {User} = require('../services/models')
 /* GET user profile. */
 router.get('/profile', function(req, res, next) {
-    res.send(req.user);
+    if(req.query.id) {
+        User.findOne({
+            where: {
+                sap: req.query.sap
+            }
+        })
+        .then((user) => {
+            if(user) {
+                res.json(user)
+            } else {
+                res.status(400).end()
+            }
+        })
+    } else res.send(req.user);
 });
 
 module.exports = router;
